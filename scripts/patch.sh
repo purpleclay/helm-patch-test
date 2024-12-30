49652f8a3dc45a12954fb6a75ceaf437ce69f7c7
purpleclay <purpleclaygh@gmail.com>
2024-12-30 07:00:57 +0000

    fix: remove macos hack to script

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i '' "s/^version\: $NSV_PREV_TAG/version\: $NSV_NEXT_TAG/" "${NSV_WORKING_DIRECTORY}/Chart.yaml"
+sed -i "s/^version\: $NSV_PREV_TAG/version\: $NSV_NEXT_TAG/" "${NSV_WORKING_DIRECTORY}/Chart.yaml"
